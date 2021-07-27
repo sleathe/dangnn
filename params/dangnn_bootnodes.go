8dedf56c16306776c07d7c37b0d241878b631b41
sleathe <cellcoin315@gmail.com>
2021-07-27 16:52:59 +0900

    add mainnet nodekey

--- params/dangnn_bootnodes.go
+++ params/dangnn_bootnodes.go
@@ -6,12 +6,13 @@ package params
 var (
 	// the main Dangnn network.
 	DangnnMainnetBootnodes = []string{
-		"enode://82d1b9a5649cafb9a75eee1d222a9b4d5ecbad86512b8905d1648585e600e4a8b0a87e1ccd6cedfc61ad75bda841827de2ddbee099fbeecf15e1497e6485ab5c@210.91.82.155:30305",
+		"enode://fb3525f1af163a0cedd18a1ebb684a4e9980397b2d4424cc957397e06fdfca7053ed46dc51ff79f64b9a043a0b9d9ca461eb223982f3f12870d4ad3ba1f91df7@3.35.184.197:30305",
+		"enode://892d0af85eb7366bbc8d966347a17836d989183a772727764ea59f2412ed693ab68344531773dc69bc83ba348ba53992993d160685dde2071df82aafbcb81ae7@15.165.163.178:30305",
 	}
 
 	// the test Dangnn network.
 	DangnnTestnetBootnodes = []string{
-		"enode://24f5adfcbef98bbfe609b4490a2bd96ac2099a9ee2b73978132603fcbddfdb911439cba35418e26c2250c9585eb85aaf1163d6c29330ac96021e68092a8bfb45@210.91.82.155:30305",	// test ip
+		"enode://82d1b9a5649cafb9a75eee1d222a9b4d5ecbad86512b8905d1648585e600e4a8b0a87e1ccd6cedfc61ad75bda841827de2ddbee099fbeecf15e1497e6485ab5c@210.91.82.155:30305", // test ip
 	}
 )
 
