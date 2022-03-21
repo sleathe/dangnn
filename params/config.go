5c92bf31f31499a014cdd1891fa051baccd0506c
sleathe <cellcoin315@gmail.com>
2022-03-21 14:10:18 +0900

    publish dg coin

--- params/config.go
+++ params/config.go
@@ -29,7 +29,7 @@ import (
 var (
 	//MainnetGenesisHash = common.HexToHash("0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3")
 	//TestnetGenesisHash = common.HexToHash("0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d")
-	MainnetGenesisHash = common.HexToHash("0xb76e75619ea281a5da17878f216170d10fe38460be445729a26eaf3c95e22ad9")
+	MainnetGenesisHash = common.HexToHash("0xa979a9020bf4bf1d8e07242fe2877202bc2125cc9b10976d4c3dc9bf1e185945")
 	TestnetGenesisHash = common.HexToHash("0xec759f17b70ff944188952b3d6253997bdfb285dd6fcacac267b579352a4f862")
 	RinkebyGenesisHash = common.HexToHash("0x6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177")
 	GoerliGenesisHash  = common.HexToHash("0xbf7e331f7f7c1dd2e05159666b3bf8bc7a8a3a9eb1d518969eab529dd9b88c1a")
